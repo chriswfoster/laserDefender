50dc8cf8243baec557de822bf8adbc8657c3ee15
chriswfoster <chriswfoster@gmail.com>
2019-10-14 23:11:39 -0500

    Pushing all progress

---
+++ Assets/Entities/EnemyFormation/Position.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour {
+
+	void OnDrawGizmos(){ // gizmos are shown within the editor, not within the game itself. 
+							//waypoints are well used for waypoints, for example. Click Gizmos in the developer screen by Camera button
+		print ("Test");
+		Debug.Log ("Hellooo");
+		Gizmos.DrawWireSphere(transform.position, 1);
+	}
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public AudioClip levelcomplete;
+	
+	public void LoadLevel(string name){
+		Application.LoadLevel(name);
+		Debug.Log ("The level you're going to load is: " + name);
+	}
+	
+	public void QuitRequest(){
+		Application.Quit();
+		Debug.Log("Quit request fired: ");
+	}
+	
+	public void LoadNextLevel(){
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	
+}
